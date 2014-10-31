02537bd895e7b07a58a730e8afffc74e0b9c0f9f
ananogal <ana.nogal@gmail.com>
2014-10-31 16:00:42 +0000

    pass tests remove_capital_letters_from_string, round_up_number, round_down_number

--- questions_spec.rb
+++ questions_spec.rb
@@ -136,17 +136,17 @@ describe 'the Friday test :)' do
     expect(n).to eq 6
   end
 
-  specify 'remove_capital_letters_from_string' do
+  fit 'remove_capital_letters_from_string' do
     n = remove_capital_letters_from_string 'Hello JohnDoe'
     expect(n).to eq 'ello ohnoe'
   end
 
-  specify 'round_up_number' do
+  fit 'round_up_number' do
     n = round_up_number 3.142
     expect(n).to eq 4
   end
 
-  specify 'round_down_number' do
+  fit 'round_down_number' do
     n = round_down_number 4.9
     expect(n).to eq 4
   end
